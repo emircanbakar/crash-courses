0091ef3048e036f3eed4c9877a3b917cacdf3045
emircanbakar <emirbakar952@gmail.com>
2024-10-24 14:51:49 +0300

    file system

---
+++ NodeJS/people.js
@@ -0,0 +1,8 @@
+const people = ["emir", "ahmet", "burak"];
+const ages = [20, 24, 27];
+console.log(people);
+
+module.exports = {
+  people: people,
+  ages: ages,
+};
---
+++ NodeJS/test.js
@@ -0,0 +1,23 @@
+// const name = "emir";
+// console.log(name);
+
+// const greet = (name) => {
+//   console.log(`hello , ${name}`);
+// };
+
+// greet("emir");
+
+// global object
+// console.log(global);
+
+// setTimeout(() => {
+//   console.log("in the timeout");
+//   clearInterval(int);
+// }, 3000);
+
+// const int = setInterval(() => {
+//   console.log("in the interval");
+// }, 1000);
+
+console.log(__dirname);
+console.log(__filename);
