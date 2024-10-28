690ddf99e43cd825dbe871b43979797f6b3ea614
emircanbakar <emirbakar952@gmail.com>
2024-10-28 19:14:46 +0300

    request , response

--- NodeJS/server.js
+++ NodeJS/server.js
@@ -3,12 +3,35 @@ const fs = require("fs");
 
 const server = http.createServer((req, res) => {
   console.log("request made", req.url, req.method);
+
   res.setHeader("Content-Type", "text/html");
-  fs.readFile("./views/index.html", (err, data) => {
+
+  let path = "./views/";
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/about-me":
+      res.statusCode = 301;
+      res.setHeader("Location", "/about");
+      res.end()
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
     if (err) {
       console.log(err);
     } else {
-    //   res.write(data);
+      //   res.write(data);
       res.end(data);
     }
   });
