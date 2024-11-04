2588a9f42d3d2d02279960b8f0bb9c1b026c93b1
emircanbakar <emirbakar952@gmail.com>
2024-11-04 15:54:54 +0300

    express routing + mvc

--- NodeJS/app.js
+++ NodeJS/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const Blog = require("./models/blog");
+const blogRoutes = require("./routes/blogRoutes");
 
 const app = express();
 
@@ -59,52 +59,10 @@ app.get("/about", (req, res) => {
   res.render("about", { title: "about" });
 });
 
+app.use("/blogs", blogRoutes);
 // app.get("/about-us", (req, res) => {
 //   res.redirect("/about");
 // });
-
-app.get("/blogs", (req, res) => {
-  Blog.find()
-    .sort({ createdAt: -1 })
-    .then((result) =>
-      res.render("index", { title: "All blogs", blogs: result })
-    )
-    .catch((err) => console.log(err));
-});
-
-app.post("/blogs", (req, res) => {
-  const blog = new Blog(req.body);
-  blog
-    .save()
-    .then((result) => {
-      res.redirect("/blogs");
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findById(id)
-    .then((result) => {
-      res.render("details", { blog: result, title: "Blog Details" });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-
-app.delete("/blogs/:id", (req, res) => {
-  const id = req.params.id;
-  Blog.findByIdAndDelete(id)
-    .then((result) => {
-      res.json({ redirect: "/blogs" });
-    })
-    .catch((err) => console.log(err));
-});
-
-app.get("/blogs/create", (req, res) => {
-  res.render("create", { title: "create" });
-});
 
 app.use((req, res) => {
   // res.status(404).sendFile("./views/404.html", { root: __dirname });
